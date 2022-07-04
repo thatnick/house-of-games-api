696d41f4a267e5600fb11323b403e84fb6c7cfa1
Nicholas Russell <thatnick@me.com>
2022-07-04 15:02:07 +0100

    Add 500 server error handler

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const app = express();
 
 app.get("/api/categories", getCategories);
 
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Server Error!");
+});
+
 module.exports = app;
